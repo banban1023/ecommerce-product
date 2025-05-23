80500ba6c32421d9673979540024585ba2998c7f
板板 <747657490@qq.com>
2025-05-23 17:56:22 +0800

    准备工作

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    Collections
+    Men
+    Women
+    About
+    Contact
+
+    Sneaker Company
+
+    Fall Limited Edition Sneakers
+
+    These low-profile sneakers are your perfect casual wear companion. Featuring a
+    durable rubber outer sole, they’ll withstand everything the weather can offer.
+
+    $125.00
+    50%
+    $250.00
+
+    0
+    Add to cart
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+</style>
